55e2ecc86c06e2aaa94a12ac52911f1cc7188b7d
darcCode <98493460+darcCode@users.noreply.github.com>
2022-01-26 17:58:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def msg_received_from_group():
   log('{}'.format(data))
   
   #Check the text of the message sent to the chat to see if it matches our command word
-  if data['text'].lower() == "hello":
-    send_msg("Hi!")
-  elif data ['text'].lower() == "who?":
-	send_msg("me")
+  if data['text'].lower() == "!test":
+    send_msg("Hello")
+  elif data ['text'].lower() == "!name":
+	send_msg("Hello " + data ['name']")
 
 	
   return "ok", 200
